35bf17b50e3b1d452202a81a162199369c38416c
Mattressburn <chevyboxer@gmail.com>
2016-08-24 15:37:06 -0400

    firstpush

--- js/app.js
+++ js/app.js
@@ -11,7 +11,52 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+  	//Function(mathrandom)
+  	//create a new game function to reset everything
+  	//validate function
+  	//function to give feedback hot or cold
+  	//tracks guesses and number of guesses
+  	
 
+  	function generateRandom() {
+  		var randomNumber = Math.floor((Math.random() * 100) + 1);
+  		return (randomNumber)
+  	}
+
+
+  	function userSubmit() {
+  		//take user input
+  		//run it through validation
+  		//give feedback
+  		//increase the count
+  		//store guess
+
+
+  	}
+
+  	function compare(userInput, winningNumber) {
+	//take user submit and compare to random number var
+		if ( abs(userInput - winningNumber) > 50 ) {
+			changeFeedback("You are ice cold")
+		}
+
+
+  	}
+
+  	function changeFeedback(string) {
+  		$("#feedback").text(string)
+  	}
+
+  	function changeCount() {
+  		var count = $("#count").text()
+  		count += count + 1
+  		$("#count").text(count)
+  	}
+
+ 	changeCount()
+
+  	// var winningNumber = generateRandom()
+  	// generateRandom();
 });
 
 
